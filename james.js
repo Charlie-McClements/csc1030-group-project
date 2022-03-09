5980004571fb33b1f530b8145067fac22335e4d0
Jmca47 <100367016+Jmca47@users.noreply.github.com>
2022-03-09 16:19:17 +0000

    Change Background Colour

--- james.js
+++ james.js
@@ -1,7 +1,11 @@
-function endScreen(){
+function endScreen() {
     stopTimer();
     document.getElementById('endScreen').className = "showMe";
     document.getElementById('gameScreen').className = "hideMe";
     document.getElementById('startScreen').className = "hideMe";
+}
 
+function changeBg() {
+    var bg = document.getElementById('bgColour').value;
+    document.body.style.backgroundColor = bg;
 }
