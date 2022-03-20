60254eb89e7e3c1ed19b4edc8c30aba4f799b74e
Charlie-McClements <cmcclements253@gmail.com>
2022-03-20 18:17:51 +0000

    end screen when timer reaches zero

--- ruari.js
+++ ruari.js
@@ -44,7 +44,7 @@ function countdown() {
         document.getElementById('seconds').innerHTML = seconds;
     } else {
         stopTimer();
-        // alert("Game Over");
+        endScreen();
     }
 }
 
