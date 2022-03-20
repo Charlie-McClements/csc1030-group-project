e61bbc8491503556b16436bbb3cc2616a255dc39
Jmca47 <100367016+Jmca47@users.noreply.github.com>
2022-03-20 15:30:22 +0000

    fixed end screen. shows time left

--- james.js
+++ james.js
@@ -1,5 +1,18 @@
 function endScreen() {
-    stopTimer();
+
+    var minutesLeft = document.getElementById('minutesLeft');
+    var secondsLeft = document.getElementById('secondsLeft');
+
+    // fetch minutes and seconds
+    var minutes = document.getElementById('minutes').innerHTML;
+    var seconds = document.getElementById('seconds').innerHTML;
+    // set mins and seconds
+    minutesLeft.innerHTML = minutes;
+    secondsLeft.innerHTML = seconds;
+
+    console.log(minutes, seconds);
+
+
     document.getElementById('endScreen').className = "showMe";
     document.getElementById('gameScreen').className = "hideMe";
     document.getElementById('startScreen').className = "hideMe";
